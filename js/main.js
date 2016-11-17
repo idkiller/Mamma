294a77520577c6f198e122470647df7bf0356555
pius lee <idkiller@gmail.com>
2016-11-17 18:48:47 +0900

    Initialize with material.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function() {
+
+    document.addEventListener('tizenhwkey', function(e) {
+        if (e.keyName === "back") {
+            try {
+                tizen.application.getCurrentApplication().exit();
+            } catch (ignore) {}
+        }
+    });
+
+})();
